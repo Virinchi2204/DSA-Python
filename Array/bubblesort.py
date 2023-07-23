d5fd5bf675e54ea978f59caca821ce5bff58dce9
Virinchi M <virinchi220403@gmail.com>
2023-07-23 21:49:25 +0530

    bubblesort python

---
+++ Array/bubblesort.py
@@ -0,0 +1,14 @@
+def bubble_sort(arr):
+    n=len(arr)
+    swapped=False
+    for i in range(n-1):
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                swapped=True
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+        if not swapped:
+            return
+input_str = input("Enter an array: ")
+array = list(map(int, input_str.split()))
+bubble_sort(array)
+print(array)
