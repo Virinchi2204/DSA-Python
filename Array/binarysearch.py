b7d6bf089eb736d1e21bfca64004880e961e042d
Virinchi M <virinchi220403@gmail.com>
2023-07-28 20:34:21 +0530

    binarysearch python

---
+++ Array/binarysearch.py
@@ -0,0 +1,21 @@
+def binarySearch(array, x, low, high):
+    while low <= high:
+        mid = low + (high - low)//2
+        if array[mid] == x:
+            return mid
+        elif array[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+input_str = input("Enter an array: ")
+array = list(map(int, input_str.split()))
+x=int(input("Enter Element to be searched: "))
+
+result = binarySearch(array, x, 0, len(array)-1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Not found")
